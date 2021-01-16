716540c0c248cbbb511c10c5a12bcf7c221d766b
Yohan <shanakaray@yahoo.com>
2021-01-16 15:33:49 +0800

    initial project structure commit

---
+++ first-bank-api/src/main/java/com/yr/atomikos/banking/FirstBankingApplication.java
@@ -0,0 +1,13 @@
+package com.yr.atomikos.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBankingApplication.class, args);
+	}
+
+}
---
+++ second-bank-api/src/main/java/com/yr/atomikos/banking/SecondBankingApplication.java
@@ -0,0 +1,13 @@
+package com.yr.atomikos.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBankingApplication.class, args);
+	}
+
+}
