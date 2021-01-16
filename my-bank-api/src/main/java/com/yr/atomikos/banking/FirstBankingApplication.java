e93ba1386682a76096c13bfdf6ff6f4e104aa5d5
Yohan <shanakaray@yahoo.com>
2021-01-16 19:32:13 +0800

    new project structure

--- my-bank-api/src/main/java/com/yr/atomikos/banking/FirstBankingApplication.java
+++ my-bank-api/src/main/java/com/yr/atomikos/banking/FirstBankingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecondBankingApplication {
+public class FirstBankingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecondBankingApplication.class, args);
+		SpringApplication.run(FirstBankingApplication.class, args);
 	}
 
 }
---
+++ my-bank-api/src/main/java/com/yr/atomikos/banking/Transfer.java
@@ -0,0 +1,7 @@
+package com.yr.atomikos.banking;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class Transfer {
+}
